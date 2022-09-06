0a7e1ef96d26adf75507772a764983e4b1b07899
shreyas <shreyas.waykar123@gmail.com>
2022-09-06 15:27:43 +0530

    Deleted Switch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import Products from "./components/Products";
 import SingleProduct from "./components/SingleProduct";
-import { Routes, Switch, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      {/* <Products /> */}
       <Routes>
         <Route path="/" element={<Products />} />
         <Route path="/:productId" element={<SingleProduct />} />
